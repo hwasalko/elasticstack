a342f0c3fcd7c66ec9b78f93ff06dc07d20b96a3
Lee Dongjin <2dong0711@hanmail.net>
2022-11-06 19:39:05 +0900

    최초커밋

---
+++ src/main/java/com/benit/elasticstack/ElasticstackApplication.java
@@ -0,0 +1,13 @@
+package com.benit.elasticstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benit/elasticstack/ElasticstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benit.elasticstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
